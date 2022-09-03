dc5fe46cc6a8672ea659585d96b67c51f60d4528
linuxlite <igor.sagitarius@gmail.com>
2022-09-03 11:23:43 -0300

    feat(EntryForm): inproving entryForm

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -6,13 +6,18 @@ import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from "./entry-list/entry-list.component";
 import { EntryFormComponent } from "./entry-form/entry-form.component";
 
+import { CalendarModule } from "primeng-lts/calendar";
+import {InputNumberModule} from 'primeng-lts/inputnumber';
+
 
 @NgModule({
   declarations: [EntryListComponent,EntryFormComponent],
   imports: [
     CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CalendarModule,
+    InputNumberModule
   ]
 })
 export class EntriesModule { }
